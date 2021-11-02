85dfe06055fbb7e4ae003c22513b8780b097a642
dvinodbabu <devarajan.vinodbabu@gmail.com>
2021-11-02 15:14:23 +0530

    added the puzzle end point

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -4,14 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from model.models import setup_db, Question, Category
+from model.models import setup_db, Question, Category,db
 
 QUESTIONS_PER_PAGE = 5
 
 
 
 def create_app(test_config=None):
-  # create and configure the app
   app = Flask(__name__)
   setup_db(app)
   cors = CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
@@ -94,82 +93,61 @@ def create_app(test_config=None):
        )
      except:
             abort(422)
+
    
   @app.route("/categories/<int:category_id>/questions", methods=["GET"])
   def get_questions_by_category(category_id):
     question = Question.query.filter(Question.category==category_id).all()
     current_questions=paginate_questions(request,question)
+    current_category=db.session.query(Category.type).filter(Category.id==category_id).all()
     return jsonify(
       {
         'questions':current_questions,
         'totalQuestions':len(question),
-        'current_category':'History'
+        'current_category': current_category
       }
     )
+
+  @app.route("/questions/query" ,methods=["POST"])
+  def question_search():
+    wild_search_question_name = "%{}%".format(request.get_json().get('searchTerm', None))
+    selection = Question.query.filter(Question.question.ilike(wild_search_question_name)).all()
+    current_questions=paginate_questions(request,selection)
+    return jsonify(
+      {
+        'questions':current_questions,
+        'totalQuestions':len(selection),
+        'current_category': None
+      }
+    )
+
+  @app.route("/quizzes", methods=["POST"])
+  def start_quiz():
+    form=request.get_json()
+    previous_questions = form.get('previous_questions')
+    category = form.get('quiz_category').get('id')
+    print(category)
+    if category==0:
+      questions = Question.query.all()
+    else:
+      questions = Question.query.filter(Question.category==category).all()
+    for question in questions:
+     if question.id not in previous_questions:
+      ask_question=question.format()
+
+    return jsonify({
+      'success':True,
+      'previousQuestions':previous_questions,
+      'currentQuestion':ask_question.format()
+    })
 
   @app.after_request
   def after_request(response):
    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTION')
    return response
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
-
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
-
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
-
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
-
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
 
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
-
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-  
   return app
 
 
