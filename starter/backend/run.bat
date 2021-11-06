5d5fcf747a20d2576daa822e5f1c85c660cf5b6e
dvinodbabu <devarajan.vinodbabu@gmail.com>
2021-11-06 10:42:19 +0530

    updated the git ignore

--- starter/backend/run.bat
+++ starter/backend/run.bat
@@ -1,7 +1,7 @@
 call dev\Scripts\activate
 set FLASK_APP=flaskr
 set FLASK_DEV=development
-set DB_USER=vinod
+set DB_USER=postgres
 set DB_PASSWORD=password
 set DB_HOST=localhost:5432
 python -m flask run
